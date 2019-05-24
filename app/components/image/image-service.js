04eac235505ac20c63b535be2cb21c568dc93a57
ben <bholsinger@gmail.com>
2019-05-24 13:16:50 -0600

    subscribers in variable and set state function

--- app/components/image/image-service.js
+++ app/components/image/image-service.js
@@ -1,11 +1,34 @@
+//Private
+
 // @ts-ignore
 const imgApi = axios.create({
 	baseURL: '//bcw-sandbox.herokuapp.com/api/images',
 	timeout: 3000
 });
 
+let _state = {
+	apiImages: [],
+	activeImage: {},
+
+}
+
+let _subscribers = {
+	apiImages: [],
+	activeImage: {},
+
+}
+
+function _setState(propName, data) {
+	_state[propName] = data
+	_subscribers[propName].forEach(fn => fn())
+}
+
+
 //currently only one image is in database
 
+
+
+//Public
 export default class ImageService {
 
 }
